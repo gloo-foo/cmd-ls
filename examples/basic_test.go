fcedaa2adcdb41b8f659e075a96ed58cc3f94367
nicerobot <nicerobot@users.noreply.github.com>
2026-07-03 15:56:02 -0700

    init

---
+++ examples/basic_test.go
@@ -0,0 +1,25 @@
+package ls_test
+
+import (
+	gloo "github.com/gloo-foo/framework/patterns"
+	"github.com/spf13/afero"
+
+	command "github.com/gloo-foo/cmd-ls"
+)
+
+func ExampleLs_basic() {
+	// ls .
+	// Uses afero.NewMemMapFs for deterministic output.
+	fs := afero.NewMemMapFs()
+	_ = afero.WriteFile(fs, "a.txt", []byte("alpha"), 0o644)
+	_ = afero.WriteFile(fs, "b.txt", []byte("beta"), 0o644)
+
+	if err := gloo.Run(
+		command.Ls(".", command.LsFs{Fs: fs}),
+	); err != nil {
+		panic(err)
+	}
+	// Output:
+	// a.txt
+	// b.txt
+}
---
+++ examples/longformat_test.go
@@ -0,0 +1,24 @@
+package ls_test
+
+import (
+	gloo "github.com/gloo-foo/framework/patterns"
+	"github.com/spf13/afero"
+
+	command "github.com/gloo-foo/cmd-ls"
+)
+
+func ExampleLs_longFormat() {
+	// ls -l .
+	fs := afero.NewMemMapFs()
+	_ = afero.WriteFile(fs, "a.txt", []byte("alpha"), 0o644)
+	_ = afero.WriteFile(fs, "b.txt", []byte("beta four bytes"), 0o644)
+
+	if err := gloo.Run(
+		command.Ls(".", command.LsFs{Fs: fs}, command.LsLongFormat),
+	); err != nil {
+		panic(err)
+	}
+	// Output:
+	// -rw-r--r-- 5 a.txt
+	// -rw-r--r-- 15 b.txt
+}
